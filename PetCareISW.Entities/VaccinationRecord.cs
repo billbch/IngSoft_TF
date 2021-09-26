6cc9867615a2ce2dec38a898c4ef64ec9cd7d0ce
Bill <bill.bchavez@gmail.com>
2021-09-26 15:02:26 -0500

    records

---
+++ PetCareISW.Entities/VaccinationRecord.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace PetCareISW.Entities
+{
+    public class VaccinationRecord
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime CreateAt { get; set; }
+
+        [Required]
+        public string VetName { get; set; }
+
+        [Required]
+        public int MedicalProfileId { get; set; }
+
+        public MedicalProfile MedicalProfile { get; set; }
+    }
+}
