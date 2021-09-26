8fea87515498d2acc5a21bc29621483fcf39292f
SebsPER <sebasphachi@gmail.com>
2021-09-26 17:07:54 -0500

    Controller, services, dtos, daos de business

---
+++ PetCareISW.DTO/BusinessDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetCareISW.DTO
+{
+    public class BusinessDto
+    {
+        public int Id { get; set; }
+        public long RUC { get; set; }
+        public string BusinessName { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string email { get; set; }
+        public float Score { get; set; }
+        public string Description { get; set; }
+    }
+}
